86fcda3023635884e761b7172dac7a57e2b48fe7
Petr Znamenskiy <petr.znamenskiy@crick.ac.uk>
2021-05-14 16:56:58 +0100

    add non-parallel option for neuropil correction

--- src/neuropil/neuropil.py
+++ src/neuropil/neuropil.py
@@ -4,28 +4,38 @@ import multiprocessing as mp
 import sys
 import os
 
-def correct_neuropil(dpath):
+def correct_neuropil(dpath, parallel=False):
     Fr = np.load(os.path.join(dpath, 'F.npy'))
     Fn = np.load(os.path.join(dpath, 'Fneu.npy'))
     stat = np.load(os.path.join(dpath, 'stat.npy'), allow_pickle=True)
 
-    cores = mp.cpu_count()
-    print('Starting parallel pool with {} processes...'.format(cores))
-    pool = mp.Pool(cores)
-
     print('Starting neuropil correction with ASt method...')
-    results = [pool.apply(ast_model, args=(
-                    np.vstack([_Fr, _Fn]),
-                    np.array([_stat['npix'], _stat['neuropil_mask'].shape[0]])
-                    )) for (_Fr, _Fn, _stat) in zip(Fr, Fn, stat)]
+    if parallel:
+        cores = mp.cpu_count()
+        print('Starting parallel pool with {} processes...'.format(cores))
+        pool = mp.Pool(cores)
+        results = [pool.apply(ast_model, args=(
+                        np.vstack([_Fr, _Fn]),
+                        np.array([_stat['npix'], _stat['neuropil_mask'].shape[0]])
+                        )) for (_Fr, _Fn, _stat) in zip(Fr, Fn, stat)]
+        pool.close()
 
-    pool.close()
+        traces, var_params = [], []
 
-    traces, var_params = [], []
-
-    for (trace, param) in results:
-        traces.append(trace)
-        var_params.append(param)
+        for (trace, param) in results:
+            traces.append(trace)
+            var_params.append(param)
+    else:
+        traces, var_params = [], []
+        for i, (_Fr, _Fn, _stat) in enumerate(zip(Fr, Fn, stat)):
+            print('Running correction for ROI {} of {}'.
+                  format(i, len(Fr)))
+            trace, param = ast_model(
+                np.vstack([_Fr, _Fn]),
+                np.array([_stat['npix'], _stat['neuropil_mask'].shape[0]])
+            )
+            traces.append(trace)
+            var_params.append(param)
 
     print('Neuropil correction completed... Saving...')
     Fast = np.vstack(traces)
