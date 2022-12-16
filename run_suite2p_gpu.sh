c818a258a42d37e908a6fe11f6fb959e536bec30
yiran25 <yiran.he@crick.ac.uk>
2022-12-16 18:29:22 +0000

    Add gpu mode memory

--- run_suite2p_gpu.sh
+++ run_suite2p_gpu.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
-#SBATCH --mem-per-cpu=G
+#SBATCH --mem-per-cpu=256G
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=END,FAIL
