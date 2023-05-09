aaed1aee466c670e5ff96a55146cd6d4fffa3069
yiran25 <yiran.he@crick.ac.uk>
2023-05-09 10:53:47 +0100

    Update jax cuda cudnn version as the previous version are not supported. Need updating it to the newest version after newer driver has been installed on camp

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ conda env create -f environment.yml
 conda activate 2p-preprocess
 conda install pip
 pip install -e .
-pip install jax[cuda11_cudnn805] -f https://storage.googleapis.com/jax-releases/jax_releases.html
+pip install "jax[cuda11_cudnn82]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 conda deactivate
