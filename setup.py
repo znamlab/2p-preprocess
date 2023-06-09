169781606aee27ea92729c07a39e697e6b26d0d3
Petr Znamenskiy <petr.znamenskiy@crick.ac.uk>
2023-06-09 10:51:47 +0100

    increment version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="2p-preprocess",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(
         where="src", include=["neuropil", "preprocess2p", "plotting_utils"]
     ),
