f7d45c2d135f0c496c3714437ae7824fdad5ac30
Caroline Cypranowska <cypranowska@gmail.com>
2024-11-06 10:16:32 +0000

    [minor] Removing extraneous print statement

--- twop_preprocess/calcium.py
+++ twop_preprocess/calcium.py
@@ -670,14 +670,20 @@ def detrend(F, first_frames, last_frames, ops, fs):
     for i, (start, end) in enumerate(zip(first_frames, last_frames)):
         rec_rolling_baseline = np.zeros_like(F[:, start:end])
         for j in range(F.shape[0]):
+            pad_front = win_frames // 2
+            pad_end = win_frames // 2 - 1
+
+            if ops["nplanes"] == 1:
+                pad_end += 1
+    
             rolling_baseline = np.pad(
                 rolling_percentile(
                     F[j, start:end],
                     win_frames,
                     ops["detrend_pctl"],
                 ),
-                pad_size,
-                mode="edge",
+                (pad_front, pad_end),
+                mode='edge',
             )
 
             rec_rolling_baseline[j, :] = rolling_baseline
