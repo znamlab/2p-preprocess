2f194d74bd8f6bd0941662d7f6e43c17d64f2b29
yiran25 <yiran.he@crick.ac.uk>
2025-03-23 12:21:29 +0000

    [minor] update nemo partition on sbatch

--- run_suite2p.sh
+++ run_suite2p.sh
@@ -5,8 +5,9 @@
 #SBATCH --ntasks=1
 #SBATCH --time=12:00:00
 #SBATCH --mem-per-cpu=4G
-#SBATCH --partition=cpu
+#SBATCH --partition=ncpu
 #SBATCH --mail-type=END,FAIL
+#SBATCH --output="/camp/lab/znamenskiyp/home/users/hey2/codes/2p-preprocess/logs/2p_preprocess_%j.log"
 ml purge
 ml CUDA/11.3.1
 ml cuDNN/8.2.1.32-CUDA-11.3.1
@@ -15,5 +16,6 @@ ml Anaconda3/2022.05
 source activate base
 
 conda activate 2p-preprocess
+
 echo Processing ${SESSION} in project ${PROJECT}
-2p calcium -p ${PROJECT} -s ${SESSION} -c append --run-neuropil y -t ${TAU} --run-split y
+2p calcium -p ${PROJECT} -s ${SESSION} -c ${CONFLICTS} --run-suite2p n --run-dff y --run-split y -t ${TAU}
