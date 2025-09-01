5cb2724db284211e6b04a11fe5cb14409b37d8b4
Caroline Cypranowska <cypranowska@gmail.com>
2025-09-01 15:10:55 +0100

    [minor] updating SLURM parameters

---
+++ register_zstack_by_name.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --job-name=register_zstack
+#SBATCH --cpus-per-task=8
+#SBATCH --ntasks=1
+#SBATCH --time=12:00:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH --partition=ncpu
+#SBATCH --mail-type=END,FAIL
+ml purge
+ml CUDA/11.3.1
+ml cuDNN/8.2.1.32-CUDA-11.3.1
+ml Anaconda3/2022.05
+
+source activate base
+
+conda activate 2p-preprocess
+
+2p zstack -p $PROJECT -s $SESSION --conflicts skip -c 0 --max-shift 50 --sequential-volumes y $STACK_NAME
